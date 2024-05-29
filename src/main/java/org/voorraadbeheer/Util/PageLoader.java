278dcf75bbcb7630cd1ce8d075221879369fd01a
ZaahirZ <zaahirziyaad@gmail.com>
2024-05-29 18:08:34 +0200

    .

--- src/main/java/org/voorraadbeheer/Util/PageLoader.java
+++ src/main/java/org/voorraadbeheer/Util/PageLoader.java
@@ -7,32 +7,53 @@ import javafx.stage.Stage;
 import org.voorraadbeheer.Main;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class PageLoader {
 
-    private static Scene scene;
-    private static Pane pane;
+    public static final String STYLESHEET_PATH = "/org/vooraadbeheer/style.css";
+    private static Pane rootLayout;
+    private static Stage stage;
+
+    public static void setRootLayout(Pane rootLayout) {
+        PageLoader.rootLayout = rootLayout;
+    }
+
+    public static void setStage(Stage stage) {
+        PageLoader.stage = stage;
+    }
 
     public static void startApplication(){
         Stage primaryStage = new Stage();
         setStage(primaryStage);
         setRootLayout(new Pane());
-        loadPage();
+        loadMainPage();
     }
 
     public static void loadPage(String fxmlName, String title) {
-        try{
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxmlFile));
-        rootLayout = fxmlLoader.load();
-        Scene scene = new Scene(rootLayout);
-        scene.getStylesheets().add(Main.class.getResource(STYLESHEET_PATH).toExternalForm());
-        stage.setScene(scene);
-        stage.setTitle(title);
-        stage.show();
-        stage.setResizable(false);
-        }
-        catch(IOException e){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxmlName));
+            Pane rootLayout = fxmlLoader.load();
+
+            if (rootLayout == null) {
+                throw new IOException("FXML file '" + fxmlName + "' could not be loaded.");
+            }
+
+            Scene scene = new Scene(rootLayout);
+            if (stage != null) {
+                stage.setScene(scene);
+                stage.setTitle(title);
+                stage.show();
+            } else {
+                System.err.println("Error: Stage is null. Cannot set scene.");
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+
+    public static void loadMainPage() {
+        loadPage("Main.fxml", "Voorraadbeheer - Startscherm");
     }
 }
